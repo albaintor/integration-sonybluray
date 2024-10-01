af287cbe92ccbf297d6028110f78f02b49c5a124
albaintor <118518828+albaintor@users.noreply.github.com>
2024-10-01 20:18:31 +0200

    Fix

--- intg-sonybluray/sonyapilib/device.py
+++ intg-sonybluray/sonyapilib/device.py
@@ -15,11 +15,10 @@ from urllib.parse import (
 )
 
 import jsonpickle
-from aiohttp import ClientTimeout, ClientResponse
+from aiohttp import ClientTimeout
 from aiohttp.web_exceptions import HTTPError
 
 import ssdp
-from xml_helper import find_in_xml
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -1079,3 +1078,43 @@ class SonyDevice:
     async def list(self):
         """Send the command 'list' to the connected device."""
         await self._send_command('List')
+
+
+def xml_search_helper(data, param):
+    """Perform find or findall on given xml with string from param."""
+    if isinstance(param, (tuple, list)) and param[1]:
+        result = data.findall(param[0])
+    else:
+        result = data.find(param)
+    return result
+
+
+def iterate_search_data(data, param):
+    """Search in nested lists."""
+    result = []
+    for element in data:
+        if isinstance(element, list):
+            result.append(iterate_search_data(element, param))
+        else:
+            result.append(xml_search_helper(element, param))
+    return result
+
+
+def find_in_xml(data, search_params):
+    """Try to find an element in an xml
+
+    Take an xml from string or as xml.etree.ElementTree
+    and an iterable of strings (and/or tuples in case of findall) to search.
+    The tuple should contain the string to search for and a true value.
+    """
+    if isinstance(data, str):
+        data = xml.etree.ElementTree.fromstring(data)
+    param = search_params[0]
+    if isinstance(data, list):
+        result = iterate_search_data(data, param)
+    else:
+        result = xml_search_helper(data, param)
+
+    if len(search_params) == 1:
+        return result
+    return find_in_xml(result, search_params[1:])
