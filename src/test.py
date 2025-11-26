271d41aaff61805778d14aee6be3f41ff4dcb746
albaintor <118518828+albaintor@users.noreply.github.com>
2025-11-26 17:34:55 +0100

    Improved connection on remote wakeup

--- src/test.py
+++ src/test.py
@@ -1,9 +1,14 @@
+# pylint: skip-file
+# flake8: noqa
 import asyncio
 import logging
 import sys
+from typing import Any
 
 from rich import print_json
 
+from client import SonyBlurayDevice, Events
+from config import DeviceInstance
 from discover import async_identify_sonybluray_devices
 from sonyapilib.device import AuthenticationResult, SonyDevice
 
@@ -15,6 +20,8 @@ if sys.platform == "win32":
 _LOOP = asyncio.new_event_loop()
 asyncio.set_event_loop(_LOOP)
 
+async def on_device_update(device_id: str, update: dict[str, Any] | None) -> None:
+    print_json(data=update)
 
 async def discover():
     devices = await async_identify_sonybluray_devices()
@@ -41,43 +48,53 @@ async def main():
     # # devices = ssdp.discover(timeout=5)
     # devices = ssdp.discover()
     # print(devices)
-    _device_config = {
-        "id": "38-18-4c-31-5a-45",
-        "name": "Sony UBP-X700",
-        "client_name": "Damien-PC",
-        "address": "192.168.1.117",
-        "always_on": False,
-        "password_key": "",
-        "app_port": 50202,
-        "dmr_port": 52323,
-        "ircc_port": 50001,
-        "mac_address": "38-18-4c-31-5a-45",
-        "pin_code": "4624",
-    }
-    _sony_device = SonyDevice(
-        host=_device_config.get("address"),
-        app_port=_device_config.get("app_port"),
-        ircc_port=_device_config.get("ircc_port"),
-        dmr_port=_device_config.get("dmr_port"),
-        psk=_device_config.get("password_key"),
-        nickname=_device_config.get("client_name"),
+    _device_config = DeviceInstance(
+        id="38-18-4c-31-5a-45",
+        name="Sony UBP-X700",
+        client_name="Damien-PC",
+        address="192.168.1.117",
+        always_on=False,
+        password_key="",
+        app_port=50202,
+        dmr_port=52323,
+        ircc_port=50001,
+        mac_address="38-18-4c-31-5a-45",
+        pin_code="4624"
     )
-    _sony_device.pin = _device_config.get("pin_code")
-    _sony_device.mac = _device_config.get("mac_address")
-    try:
-        await _sony_device.init_device()
-    except Exception:
-        print("Exception")
-    register_result = await _sony_device.register()
-    if register_result == AuthenticationResult.PIN_NEEDED:
-        print("PIN NEEDED")
-    else:
-        print("NO PIN NEEDED")
-    status = await _sony_device.get_power_status(timeout=2)
-    if status:
-        print("ON")
-    else:
-        print("OFF")
+    client = SonyBlurayDevice(
+        device_config=_device_config
+    )
+    client.events.on(Events.UPDATE, on_device_update)
+    await client.connect()
+    await client.turn_on()
+    await client.send_key("Right")
+    await asyncio.sleep(100)
+
+
+    # _sony_device = SonyDevice(
+    #     host=_device_config.get("address"),
+    #     app_port=_device_config.get("app_port"),
+    #     ircc_port=_device_config.get("ircc_port"),
+    #     dmr_port=_device_config.get("dmr_port"),
+    #     psk=_device_config.get("password_key"),
+    #     nickname=_device_config.get("client_name"),
+    # )
+    # _sony_device.pin = _device_config.get("pin_code")
+    # _sony_device.mac = _device_config.get("mac_address")
+    # try:
+    #     await _sony_device.init_device()
+    # except Exception:
+    #     print("Exception")
+    # register_result = await _sony_device.register()
+    # if register_result == AuthenticationResult.PIN_NEEDED:
+    #     print("PIN NEEDED")
+    # else:
+    #     print("NO PIN NEEDED")
+    # status = await _sony_device.get_power_status(timeout=2)
+    # if status:
+    #     print("ON")
+    # else:
+    #     print("OFF")
     # _sony_device.power(True)
 
 
